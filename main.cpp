f095873b5e6df132152163b84cbcc2dd40e576f6
Raghuveer Devulapalli <raghuveer.devulapalli@intel.com>
2025-01-31 09:58:57 -0800

    SSE4.2 optimzied version works

--- main.cpp
+++ main.cpp
@@ -3,14 +3,30 @@
 #include <wmmintrin.h>
 #include <immintrin.h>
 #include <gtest/gtest.h>
+#include <zlib.h>
 
 #define zalign(x) __attribute__((aligned(x)))
 
 __attribute__((target("sse4.2")))
-uint32_t crc32c_sse42(  /* SSE4.2+PCLMUL */
-    const unsigned char *buf,
-    size_t len,
-    uint32_t crc)
+inline uint32_t
+crc32c_scalar(const unsigned char *data, ssize_t len, uint32_t crc)
+{
+    const unsigned char *p = data;
+    const unsigned char *pend = p + len;
+
+    /* go byte by byte: */
+    while (p < pend)
+    {
+        crc = _mm_crc32_u8(crc, *p);
+        p++;
+    }
+
+    return crc;
+}
+
+__attribute__((target("sse4.2")))
+inline uint32_t
+crc32c_sse42(const unsigned char *buf, ssize_t len, uint32_t crc)
 {
     /*
      * Definitions of the bit-reflected domain constants k1,k2,k3, etc and
@@ -20,123 +36,121 @@ uint32_t crc32c_sse42(  /* SSE4.2+PCLMUL */
     static const uint64_t zalign(16) k3k4[] = { 0xf20c0dfe, 0x14cd00bd6 };
     static const uint64_t zalign(16) k5k0[] = { 0xdd45aab8, 0x000000000 };
     static const uint64_t zalign(16) poly[] = { 0x105ec76f1, 0xdea713f1 };
-    __m128i x0, x1, x2, x3, x4, x5, x6, x7, x8, y5, y6, y7, y8;
-    /*
-     * There's at least one block of 64.
-     */
-    x1 = _mm_loadu_si128((__m128i *)(buf + 0x00));
-    x2 = _mm_loadu_si128((__m128i *)(buf + 0x10));
-    x3 = _mm_loadu_si128((__m128i *)(buf + 0x20));
-    x4 = _mm_loadu_si128((__m128i *)(buf + 0x30));
-    x1 = _mm_xor_si128(x1, _mm_cvtsi32_si128(crc));
-    x0 = _mm_load_si128((__m128i *)k1k2);
-    buf += 64;
-    len -= 64;
-    /*
-     * Parallel fold blocks of 64, if any.
-     */
-    while (len >= 64)
-    {
-        x5 = _mm_clmulepi64_si128(x1, x0, 0x00);
-        x6 = _mm_clmulepi64_si128(x2, x0, 0x00);
-        x7 = _mm_clmulepi64_si128(x3, x0, 0x00);
-        x8 = _mm_clmulepi64_si128(x4, x0, 0x00);
-        x1 = _mm_clmulepi64_si128(x1, x0, 0x11);
-        x2 = _mm_clmulepi64_si128(x2, x0, 0x11);
-        x3 = _mm_clmulepi64_si128(x3, x0, 0x11);
-        x4 = _mm_clmulepi64_si128(x4, x0, 0x11);
-        y5 = _mm_loadu_si128((__m128i *)(buf + 0x00));
-        y6 = _mm_loadu_si128((__m128i *)(buf + 0x10));
-        y7 = _mm_loadu_si128((__m128i *)(buf + 0x20));
-        y8 = _mm_loadu_si128((__m128i *)(buf + 0x30));
-        x1 = _mm_xor_si128(x1, x5);
-        x2 = _mm_xor_si128(x2, x6);
-        x3 = _mm_xor_si128(x3, x7);
-        x4 = _mm_xor_si128(x4, x8);
-        x1 = _mm_xor_si128(x1, y5);
-        x2 = _mm_xor_si128(x2, y6);
-        x3 = _mm_xor_si128(x3, y7);
-        x4 = _mm_xor_si128(x4, y8);
+    if (len >= 64) {
+        __m128i x0, x1, x2, x3, x4, x5, x6, x7, x8, y5, y6, y7, y8;
+        /*
+         * There's at least one block of 64.
+         */
+        x1 = _mm_loadu_si128((__m128i *)(buf + 0x00));
+        x2 = _mm_loadu_si128((__m128i *)(buf + 0x10));
+        x3 = _mm_loadu_si128((__m128i *)(buf + 0x20));
+        x4 = _mm_loadu_si128((__m128i *)(buf + 0x30));
+        x1 = _mm_xor_si128(x1, _mm_cvtsi32_si128(crc));
+        x0 = _mm_load_si128((__m128i *)k1k2);
         buf += 64;
         len -= 64;
-    }
-    /*
-     * Fold into 128-bits.
-     */
-    x0 = _mm_load_si128((__m128i *)k3k4);
-    x5 = _mm_clmulepi64_si128(x1, x0, 0x00);
-    x1 = _mm_clmulepi64_si128(x1, x0, 0x11);
-    x1 = _mm_xor_si128(x1, x2);
-    x1 = _mm_xor_si128(x1, x5);
-    x5 = _mm_clmulepi64_si128(x1, x0, 0x00);
-    x1 = _mm_clmulepi64_si128(x1, x0, 0x11);
-    x1 = _mm_xor_si128(x1, x3);
-    x1 = _mm_xor_si128(x1, x5);
-    x5 = _mm_clmulepi64_si128(x1, x0, 0x00);
-    x1 = _mm_clmulepi64_si128(x1, x0, 0x11);
-    x1 = _mm_xor_si128(x1, x4);
-    x1 = _mm_xor_si128(x1, x5);
-    /*
-     * Single fold blocks of 16, if any.
-     */
-    while (len >= 16)
-    {
-        x2 = _mm_loadu_si128((__m128i *)buf);
+        /*
+         * Parallel fold blocks of 64, if any.
+         */
+        while (len >= 64)
+        {
+            x5 = _mm_clmulepi64_si128(x1, x0, 0x00);
+            x6 = _mm_clmulepi64_si128(x2, x0, 0x00);
+            x7 = _mm_clmulepi64_si128(x3, x0, 0x00);
+            x8 = _mm_clmulepi64_si128(x4, x0, 0x00);
+            x1 = _mm_clmulepi64_si128(x1, x0, 0x11);
+            x2 = _mm_clmulepi64_si128(x2, x0, 0x11);
+            x3 = _mm_clmulepi64_si128(x3, x0, 0x11);
+            x4 = _mm_clmulepi64_si128(x4, x0, 0x11);
+            y5 = _mm_loadu_si128((__m128i *)(buf + 0x00));
+            y6 = _mm_loadu_si128((__m128i *)(buf + 0x10));
+            y7 = _mm_loadu_si128((__m128i *)(buf + 0x20));
+            y8 = _mm_loadu_si128((__m128i *)(buf + 0x30));
+            x1 = _mm_xor_si128(x1, x5);
+            x2 = _mm_xor_si128(x2, x6);
+            x3 = _mm_xor_si128(x3, x7);
+            x4 = _mm_xor_si128(x4, x8);
+            x1 = _mm_xor_si128(x1, y5);
+            x2 = _mm_xor_si128(x2, y6);
+            x3 = _mm_xor_si128(x3, y7);
+            x4 = _mm_xor_si128(x4, y8);
+            buf += 64;
+            len -= 64;
+        }
+        /*
+         * Fold into 128-bits.
+         */
+        x0 = _mm_load_si128((__m128i *)k3k4);
         x5 = _mm_clmulepi64_si128(x1, x0, 0x00);
         x1 = _mm_clmulepi64_si128(x1, x0, 0x11);
         x1 = _mm_xor_si128(x1, x2);
         x1 = _mm_xor_si128(x1, x5);
-        buf += 16;
-        len -= 16;
+        x5 = _mm_clmulepi64_si128(x1, x0, 0x00);
+        x1 = _mm_clmulepi64_si128(x1, x0, 0x11);
+        x1 = _mm_xor_si128(x1, x3);
+        x1 = _mm_xor_si128(x1, x5);
+        x5 = _mm_clmulepi64_si128(x1, x0, 0x00);
+        x1 = _mm_clmulepi64_si128(x1, x0, 0x11);
+        x1 = _mm_xor_si128(x1, x4);
+        x1 = _mm_xor_si128(x1, x5);
+        /*
+         * Single fold blocks of 16, if any.
+         */
+        while (len >= 16)
+        {
+            x2 = _mm_loadu_si128((__m128i *)buf);
+            x5 = _mm_clmulepi64_si128(x1, x0, 0x00);
+            x1 = _mm_clmulepi64_si128(x1, x0, 0x11);
+            x1 = _mm_xor_si128(x1, x2);
+            x1 = _mm_xor_si128(x1, x5);
+            buf += 16;
+            len -= 16;
+        }
+        /*
+         * Fold 128-bits to 64-bits.
+         */
+        x2 = _mm_clmulepi64_si128(x1, x0, 0x10);
+        x3 = _mm_setr_epi32(~0, 0, ~0, 0);
+        x1 = _mm_srli_si128(x1, 8);
+        x1 = _mm_xor_si128(x1, x2);
+        x0 = _mm_loadl_epi64((__m128i*)k5k0);
+        x2 = _mm_srli_si128(x1, 4);
+        x1 = _mm_and_si128(x1, x3);
+        x1 = _mm_clmulepi64_si128(x1, x0, 0x00);
+        x1 = _mm_xor_si128(x1, x2);
+        /*
+         * Barret reduce to 32-bits.
+         */
+        x0 = _mm_load_si128((__m128i*)poly);
+        x2 = _mm_and_si128(x1, x3);
+        x2 = _mm_clmulepi64_si128(x2, x0, 0x10);
+        x2 = _mm_and_si128(x2, x3);
+        x2 = _mm_clmulepi64_si128(x2, x0, 0x00);
+        x1 = _mm_xor_si128(x1, x2);
+        crc = _mm_extract_epi32(x1, 1);
     }
-    /*
-     * Fold 128-bits to 64-bits.
-     */
-    x2 = _mm_clmulepi64_si128(x1, x0, 0x10);
-    x3 = _mm_setr_epi32(~0, 0, ~0, 0);
-    x1 = _mm_srli_si128(x1, 8);
-    x1 = _mm_xor_si128(x1, x2);
-    x0 = _mm_loadl_epi64((__m128i*)k5k0);
-    x2 = _mm_srli_si128(x1, 4);
-    x1 = _mm_and_si128(x1, x3);
-    x1 = _mm_clmulepi64_si128(x1, x0, 0x00);
-    x1 = _mm_xor_si128(x1, x2);
-    /*
-     * Barret reduce to 32-bits.
-     */
-    x0 = _mm_load_si128((__m128i*)poly);
-    x2 = _mm_and_si128(x1, x3);
-    x2 = _mm_clmulepi64_si128(x2, x0, 0x10);
-    x2 = _mm_and_si128(x2, x3);
-    x2 = _mm_clmulepi64_si128(x2, x0, 0x00);
-    x1 = _mm_xor_si128(x1, x2);
-    /*
-     * Return the crc32.
-     */
-    return _mm_extract_epi32(x1, 1);
+    return crc32c_scalar(buf, len, crc);
 }
+
 
 __attribute__((target("avx512vl,vpclmulqdq")))
 inline uint32_t
-crc32c_avx512(
-    const unsigned char *buf,
-    size_t length,
-    uint32_t crc)
+crc32c_avx512(const unsigned char* data, ssize_t length, uint32_t crc)
 {
-	static const uint64_t k1k2[8] = {
+	static const uint64_t zalign(64) k1k2[8] = {
 		0xdcb17aa4, 0xb9e02b86, 0xdcb17aa4, 0xb9e02b86, 0xdcb17aa4,
 		0xb9e02b86, 0xdcb17aa4, 0xb9e02b86};
-	static const uint64_t k3k4[8] = {
+	static const uint64_t zalign(64) k3k4[8] = {
 		0x740eef02, 0x9e4addf8, 0x740eef02, 0x9e4addf8, 0x740eef02,
 		0x9e4addf8, 0x740eef02, 0x9e4addf8};
-	static const uint64_t k9k10[8] = {
+	static const uint64_t zalign(64) k9k10[8] = {
 		0x6992cea2, 0x0d3b6092, 0x6992cea2, 0x0d3b6092, 0x6992cea2,
 		0x0d3b6092, 0x6992cea2, 0x0d3b6092};
-	static const uint64_t k1k4[8] = {
+	static const uint64_t zalign(64) k1k4[8] = {
 		0x1c291d04, 0xddc0152b, 0x3da6d0cb, 0xba4fc28e, 0xf20c0dfe,
 		0x493c7d27, 0x00000000, 0x00000000};
 
-	const uint8_t *input = (const uint8_t *) buf;
+	const uint8_t *input = (const uint8_t *)data;
 	if (length >= 256)
 	{
 		uint64_t val;
@@ -252,34 +266,45 @@ crc32c_avx512(
 	}
 
 	/*
-	 * Finish any remaining bytes with legacy AVX algorithm.
+	 * Finish any remaining bytes with a simple crc32c computation
 	 */
-	return crc32c_sse42(input, length, crc);
+	return crc32c_scalar(input, length, crc);
 }
 
-template<typename T>
-static T randomval(T min, T max)
+static uint8_t randomval()
 {
-    return (rand() / RAND_MAX) * (max - min) + min;
+    return (rand() % 255);
 }
 
-class crc32c :public ::testing::TestWithParam<int> {
+class test_crc32c :public ::testing::TestWithParam<int> {
 };
 
-TEST_P(crc32c, postgres) {
+TEST_P(test_crc32c, sse_scalar) {
     int size = GetParam();
-    std::vector<uint8_t> arr;
+    std::vector<unsigned char> arr;
     srand(42);
     for (size_t ii = 0; ii < size; ++ii) {
-        arr.push_back(randomval(0, 255));
+        arr.push_back(randomval());
     }
     uint32_t ssecrc = crc32c_sse42(arr.data(), size, 0xFFFFFFFF);
+    uint32_t sse_pg_crc = crc32c_scalar(arr.data(), size, 0xFFFFFFFF);
+    ASSERT_EQ(sse_pg_crc, ssecrc);
+}
+
+TEST_P(test_crc32c, avx512_scalar) {
+    int size = GetParam();
+    std::vector<unsigned char> arr;
+    srand(42);
+    for (size_t ii = 0; ii < size; ++ii) {
+        arr.push_back(randomval());
+    }
     uint32_t avxcrc = crc32c_avx512(arr.data(), size, 0xFFFFFFFF);
-    ASSERT_EQ(avxcrc, ssecrc);
+    uint32_t sse_pg_crc = crc32c_scalar(arr.data(), size, 0xFFFFFFFF);
+    ASSERT_EQ(sse_pg_crc, avxcrc);
 }
 
 INSTANTIATE_TEST_CASE_P(
         crc32ctests,
-	crc32c,
-        ::testing::Values(256, 511, 512));
+	test_crc32c,
+        ::testing::Values(1, 2, 3, 4, 5, 6, 7, 8, 9, 16, 17, 18, 19, 32, 64, 65, 66, 71, 128, 256));
 
